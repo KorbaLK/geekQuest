d1d93dfc731af26af22e48492e57d30c2b46b255
KorbaLK <lukas-korber@web.de>
2015-03-31 17:38:10 +0200

    Initial

---
+++ src/geekquest/GeekQuestServlet.java
@@ -0,0 +1,13 @@
+package geekquest;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GeekQuestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
